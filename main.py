d6c2b70dc61d806c7ec497af8974d1b1de803c19
mhmtgrmz12 <148793358+mhmtgrmz12@users.noreply.github.com>
2025-05-06 13:04:27 +0200

    CORS updated

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import config
+from fastapi.middleware.cors import CORSMiddleware
 import endpoints
 
 app = FastAPI()
 
 app.include_router(endpoints.router)
+
+origins = config.cors_origins.split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
